327968a9d63522af871fbbf150c2b8e4975e01bc
Shaik Shabber <87518304+Shabber1412@users.noreply.github.com>
2022-07-25 18:29:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
     region = "us-east-2"
-    access_key = "AKIA2YTZ462YIGZ3KBXD"
-    secret_key = "4Zc0WBOxSvrol5fQiUsDMdo5cL3oMYmFnCFSxDJf"
+    access_key = "Your Access Key"
+    secret_key = "Your Secret Key"
 }

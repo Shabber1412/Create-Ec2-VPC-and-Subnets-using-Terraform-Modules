4ddd2bb271b5dbd1d3b1fd1ae8e590328090aff8
Shaik Shabber <87518304+Shabber1412@users.noreply.github.com>
2022-07-25 18:53:55 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,23 @@
+resource "aws_vpc" "vpc" {
+  cidr_block       = "192.168.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "My_VPC"
+  }
+}
 
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "192.168.1.0/24"
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "192.168.3.0/24"
+  tags = {
+    Name = "private"
+  }
+  }
